74d9e2ce00d8dfc25693b8d15f21ef1e893d0efc
Hai-Zhuan Yuan <yhz@xtu.edu.cn>
2020-02-22 21:41:03 +0800

    first commit

---
+++ Makefile
@@ -0,0 +1,83 @@
+# Makefile for XtuThesis
+
+PACKAGE = xtuthesis
+THESIS  = main
+SPINE   = spine
+
+SOURCES = $(PACKAGE).ins $(PACKAGE).dtx
+CLSFILE = dtx-style.sty $(PACKAGE).cls
+
+LATEXMK = xelatex #latexmk
+
+# make deletion work on Windows
+ifdef SystemRoot
+	RM = del /Q
+else
+	RM = rm -f
+endif
+
+.PHONY: all all-dev clean distclean dist thesis viewthesis spine viewspine doc viewdoc cls check save savepdf test FORCE_MAKE
+
+thesis: $(THESIS).pdf
+
+all: thesis spine
+
+all-dev: doc all
+
+cls: $(CLSFILE)
+
+$(CLSFILE): $(SOURCES)
+	xetex $(PACKAGE).ins
+
+doc: $(PACKAGE).pdf
+
+spine: $(SPINE).pdf
+
+$(PACKAGE).pdf: cls FORCE_MAKE
+	$(LATEXMK) $(PACKAGE).dtx
+
+$(THESIS).pdf: cls FORCE_MAKE
+	$(LATEXMK) $(THESIS)
+
+$(SPINE).pdf: cls FORCE_MAKE
+	$(LATEXMK) $(SPINE)
+
+viewdoc: doc
+	$(LATEXMK) -pv $(PACKAGE).dtx
+
+viewthesis: thesis
+	$(LATEXMK) -pv $(THESIS)
+
+viewspine: spine
+	$(LATEXMK) -pv $(SPINE)
+
+save:
+	bash testfiles/save.sh
+
+savepdf:
+	bash testfiles/save-pdf.sh
+
+test:
+	l3build check
+
+clean:
+#	$(LATEXMK) -c $(PACKAGE).dtx $(THESIS) $(SPINE)
+	-@$(RM) *~ main-survey.*
+	-@$(RM) *.aux *.toc *.log *.fdb_latexmk *.out *.thm *.cls *.hd *.idx
+
+cleanall: clean
+	-@$(RM) $(PACKAGE).pdf $(THESIS).pdf $(SPINE).pdf
+
+distclean: # cleanall
+	-@$(RM) *.aux *.toc *.log *.fdb_latexmk *.out *.thm *.cls *.hd *.idx *.pdf
+
+check: FORCE_MAKE
+ifeq ($(version),)
+	@echo "Error: version missing: \"make [check|dist] version=X.Y.Z\""; exit 1
+else
+	@[[ $(shell grep -E -c '$(version) Xiangtan University Thesis Template|\\def\\version\{$(version)\}' xtuthesis.dtx) -eq 3 ]] || (echo "update version in xtuthesis.dtx before release"; exit 1)
+	@[[ $(shell grep -E -c '"version": "$(version)"' package.json) -eq 1 ]] || (echo "update version in package.json before release"; exit 1)
+endif
+
+dist: check all-dev
+	npm run build -- --version=$(version)
---
+++ gulpfile.js
@@ -0,0 +1,128 @@
+const argv = require('minimist')(process.argv.slice(2))
+const path = require('path');
+const gulp = require('gulp');
+const log = require('fancy-log');
+const color = require('ansi-colors');
+const del = require('del');
+const zip = require('gulp-zip');
+
+const packageName = 'xtuthesis';
+
+const config = {
+    template: {
+        files: ['xtuthesis.ins',
+                'xtuthesis.dtx',
+                'tsinghua.pdf',
+                'xtuthesis-numeric.bst',
+                'xtuthesis-author-year.bst',
+                'xtuthesis-bachelor.bst',
+                'Makefile',
+                'latexmkrc',
+                'README.md',
+                'xtuthesis.pdf'],
+        // generated shold not be included for ctan archive
+        generated: ['xtuthesis.cls']
+    },
+
+    example: {
+        files: ['main.tex',
+                'spine.tex',
+                'main.pdf',
+                'spine.pdf',
+                'xtusetup.tex',
+                'math_commands.tex',
+                'data/*.tex',
+                'figures/*.*'
+                'logos/*.*',
+                'ref/*.bib']
+    },
+
+    dist: {
+        root: './dist',
+        files: [],
+        build: '',
+        zip: ''
+    },
+};
+
+function usage() {
+    log('Usage:');
+    log('\t make dist version=x.y.z');
+}
+
+function _default(callback) {
+    usage();
+
+    callback();
+}
+
+function bootstrap(callback) {
+    if (!argv.hasOwnProperty('version')) {
+        usage();
+
+        process.exit(1);
+    }
+
+    callback();
+}
+
+function cleanup(callback) {
+    del.sync([path.join(config.dist.root, config.dist.build)]);
+
+    log(color.green.bold(`🍺 ${config.dist.zip} generated`));
+
+    callback();
+}
+
+function copy(callback) {
+    const src = config.dist.files;
+    const dest = path.join(config.dist.root, config.dist.build);
+
+    return gulp.src(src, {
+        cwdbase: true
+    })
+        .pipe(gulp.dest(dest));
+}
+
+function compress(callback) {
+    const src = path.join(config.dist.build, '**/*');
+
+    return gulp.src(src, {
+        cwd: config.dist.root,
+        cwdbase: true
+    })
+        .pipe(zip(config.dist.zip))
+        .pipe(gulp.dest(config.dist.root));
+}
+
+function init_self(callback) {
+    config.dist.files = [...config.template.files, ...config.example.files, ...config.template.generated];
+    config.dist.build = `${packageName}-v${argv.version}`;
+    config.dist.zip = `${config.dist.build}.zip`;
+
+    log(`Removing old ${config.dist.build}...`);
+    del.sync([path.join(config.dist.root, config.dist.build),
+              path.join(config.dist.root, config.dist.zip)]);
+
+    callback();
+}
+
+function init_ctan(callback) {
+    config.dist.files = [...config.template.files, ...config.example.files];
+    config.dist.build = `${packageName}`;
+    config.dist.zip = `${config.dist.build}.zip`;
+
+    log(`Removing old ${config.dist.build}...`);
+    del.sync([path.join(config.dist.root, config.dist.build),
+              path.join(config.dist.root, config.dist.zip)]);
+
+    callback();
+}
+
+const build_self = gulp.series(init_self, copy, compress, cleanup);
+const build_ctan = gulp.series(init_ctan, copy, compress, cleanup);
+
+const build = gulp.series(bootstrap, build_self, build_ctan);
+
+exports.default = _default;
+exports.build = build;
